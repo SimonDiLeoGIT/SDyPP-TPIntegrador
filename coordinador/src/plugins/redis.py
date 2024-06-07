2f184613ed1141603c443c9bbf1c1fb572ce93db
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-06-07 10:00:26 -0300

    agrega reintento en caso de que falle la conexión con redis

--- coordinador/src/plugins/redis.py
+++ coordinador/src/plugins/redis.py
@@ -1,9 +1,27 @@
 import redis
 import os
+import time
+import redis.exceptions
 
 
 def redis_connect():
     redis_port = os.environ.get('REDIS_PORT')
     redis_host = os.environ.get('REDIS_HOST')
 
-    return redis.Redis(host=redis_host, port=redis_port, decode_responses=True)
+    def connect():
+        try:
+            connection = redis.Redis(
+                host=redis_host, port=redis_port, decode_responses=True)
+
+            return connection
+        except redis.exceptions.ConnectionError:
+            return None
+
+    while True:
+        connection = connect()
+        if connection:
+            break
+        print("Failed to connect to Redis. Retrying in 5 seconds...")
+        time.sleep(5)
+
+    return connection
