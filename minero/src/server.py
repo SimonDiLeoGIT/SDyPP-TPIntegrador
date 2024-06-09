e215f04d753f6ae4bef479487b555a94f921be85
Neielmonti <neielmonti@gmail.com>
2024-06-09 18:31:01 -0300

    minero cpu

--- minero/src/server.py
+++ minero/src/server.py
@@ -1,17 +1,18 @@
 import ast
-import sys
-import time
-import threading
-import requests
+import json
 import os
-from plugins.rabbitmq import rabbit_connect
+import sys
+import threading
+import time
+
+import requests
 from flask import Flask, jsonify
 from model.block import Block
-import json
-
+from plugins.rabbitmq import rabbit_connect
+from utils.check_gpu import check_for_nvidia_smi
+from utils.find_nonce import find_nonce_with_prefix
 
 app = Flask(__name__)
-# logging.basicConfig(level=logging.DEBUG)
 
 
 # Variables globales para mantener las conexiones
@@ -25,51 +26,6 @@ def status():
         "status": "200",
         "description": "Coordinator proccess is executing..."
     })
-
-
-# @ app.route("/result", methods=['POST'])
-# def validateBlock():
-#     try:
-#         block = ast.literal_eval(request.get_data().decode("utf-8"))
-
-#         timestamp = block["timestamp"]
-#         block_hash = block["hash"]
-#         previous_hash = block["previous_hash"]
-#         data = block["data"]
-#         index = block["index"]
-#         nonce = block["nonce"]
-
-#         new_block = Block(
-#             data, timestamp, block_hash, previous_hash, nonce, index)
-
-#         # TODO => Validar el bloque
-#         new_block.validate()
-
-#         # TODO => Verificar si existe en redis. Si no existe almacenarlo. Si ya existe descarto está request, porque ya un minero completo la tarea antes
-#         # Guardo el indice del nuevo bloque en el sorted set
-#         redis.zadd('blockchain', {new_block.hash: time.time()})
-#         # Guardo el bloque en la blockchain, asociandoló con el bloque anterior
-#         block_id = f"block:{new_block.previous_hash}"
-#         redis.hset(block_id, mapping=new_block.to_dict())
-
-#         return jsonify({
-#             "status": "200",
-#             "description": f"Block {new_block.index} created",
-#             "block_data": new_block.to_dict()
-#         })
-
-#     except redis_exceptions.RedisError as error:
-#         print(f"Redis error: {error}", file=sys.stderr, flush=True)
-#         return jsonify({
-#             "status": "500",
-#             "description": "Internal server error"
-#         })
-#     except Exception as e:
-#         print(f"Unexpected error: {e}", file=sys.stderr, flush=True)
-#         return jsonify({
-#             "status": "500",
-#             "description": "Internal server error"
-#         })
 
 def consume_tasks():
     print(" [*] Waiting for messages. To exit press CTRL+C")
@@ -87,13 +43,20 @@ def consume_tasks():
 
         block = Block(block["data"], block["timestamp"],
                       block_hash, block["previous_hash"], nonce, block["index"])
+        
+        if gpu_available:
+            print("GPU available")
+            # TODO: Invocar al minero_cuda para que calcule el hash y el nonce
+            block_hash = challenge + "XXXXXXXX"
+            nonce = 23
+        else: 
+            print("GPU no available")
+            block_content = block.get_block_content_as_string()
+            nonce, block_hash = find_nonce_with_prefix(challenge, block_content, 0, 1000000)
 
-        # TODO => Invocar al minero_cuda para que calcule el hash y el nonce
-        block_content = block.get_block_content_as_string()
-        block_hash = challenge + "XXXXXXXX"
-        nonce = 23
+        print(f"block_hash: {block_hash}", file=sys.stdout, flush=True)
+        print(f"nonce: {nonce}", file=sys.stdout, flush=True)
 
-        print(block_content, file=sys.stdout, flush=True)
         # Actualiza el bloque con los valores calculador por el minero_cuda
         block.hash = block_hash
         block.nonce = nonce
@@ -116,5 +79,6 @@ def consume_tasks():
 
 # Iniciar el consumidor al arrancar la aplicación Flask
 time.sleep(5)
+gpu_available = check_for_nvidia_smi()
 consumer_thread = threading.Thread(target=consume_tasks)
 consumer_thread.start()
---
+++ minero/src/utils/find_nonce.py
@@ -0,0 +1,42 @@
+import hashlib
+
+def find_nonce_with_prefix(target_hash_prefix, base_string, start_nonce, end_nonce):
+    """
+    Encuentra un nonce tal que el hash MD5 del (base_string + nonce) comience con target_hash_prefix.
+    Busca el nonce dentro del rango [start_nonce, end_nonce].
+    
+    Parameters:
+    - target_hash_prefix: Prefijo del hash objetivo.
+    - base_string: Cadena base a la que se concatenará el nonce.
+    - start_nonce: Inicio del rango de nonces.
+    - end_nonce: Fin del rango de nonces.
+    
+    Returns:
+    - (nonce, hash): Nonce encontrado y el hash correspondiente.
+    - None: Si no se encuentra ningún nonce que cumpla con la condición.
+    """
+    for nonce in range(start_nonce, end_nonce + 1):
+        test_string = f"{base_string}{nonce}"
+        hash_result = hashlib.md5(test_string.encode()).hexdigest()
+        if hash_result.startswith(target_hash_prefix):
+            return nonce, hash_result
+    return None
+
+def main(target_hash_prefix, base_string, start_nonce, end_nonce):
+    result = find_nonce_with_prefix(target_hash_prefix, base_string, start_nonce, end_nonce)
+    
+    if result:
+        nonce, hash_result = result
+        return f"Nonce encontrado: {nonce}\nHash correspondiente: {hash_result}"
+    else:
+        return "No se encontró ningún nonce que cumpla con la condición en el rango proporcionado."
+
+# Ejemplo de uso
+if __name__ == "__main__":
+    target_hash_prefix = "0000"
+    base_string = "example"
+    start_nonce = 0
+    end_nonce = 1000000
+
+    result = main(target_hash_prefix, base_string, start_nonce, end_nonce)
+    print(result)
