f43ace83c9218c8a59c4bc193ad7ffb918d1517a
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-06-17 16:21:49 -0300

    build

--- minero/src/utils/check_gpu.py
+++ minero/src/utils/check_gpu.py
@@ -1,4 +1,5 @@
 import subprocess
+import sys
 
 
 def check_for_nvidia_smi():
@@ -6,9 +7,10 @@ def check_for_nvidia_smi():
         result = subprocess.run(
             ['nvidia-smi'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         if result.returncode == 0:
-            print(result.stdout.decode('utf-8'))
+            print(result.stdout.decode('utf-8'), file=sys.stderr, flush=True)
             return True
         else:
+            print(result.stdout.decode('utf-8'), file=sys.stderr, flush=True)
             return False
     except FileNotFoundError:
         return False
